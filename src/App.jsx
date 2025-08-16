740eec323633807a0ebb9cc74af8d390f0f6e0a7
miguelr215 <drexelramos@gmail.com>
2025-08-16 15:34:27 -0400

    refactor context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
+import { PostProvider, PostContext } from './PostContext';
 
 function createRandomPost() {
 	return {
@@ -8,35 +9,10 @@ function createRandomPost() {
 	};
 }
 
-// 1) Create Context  (this is technically a component)
-const PostContext = createContext();
-
 function App() {
-	const [posts, setPosts] = useState(() =>
-		Array.from({ length: 30 }, () => createRandomPost())
-	);
-	const [searchQuery, setSearchQuery] = useState('');
+	// Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
 	const [isFakeDark, setIsFakeDark] = useState(false);
 
-	// Derived state. These are the posts that will actually be displayed
-	const searchedPosts =
-		searchQuery.length > 0
-			? posts.filter((post) =>
-					`${post.title} ${post.body}`
-						.toLowerCase()
-						.includes(searchQuery.toLowerCase())
-			  )
-			: posts;
-
-	function handleAddPost(post) {
-		setPosts((posts) => [post, ...posts]);
-	}
-
-	function handleClearPosts() {
-		setPosts([]);
-	}
-
-	// Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
 	useEffect(
 		function () {
 			document.documentElement.classList.toggle('fake-dark-mode');
@@ -46,15 +22,7 @@ function App() {
 
 	return (
 		// 2) Provide value to child components
-		<PostContext.Provider
-			value={{
-				posts: searchedPosts,
-				onAddPost: handleAddPost,
-				onClearPosts: handleClearPosts,
-				searchQuery,
-				setSearchQuery,
-			}}
-		>
+		<PostProvider>
 			<section>
 				<button
 					onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
@@ -68,7 +36,7 @@ function App() {
 				<Archive />
 				<Footer />
 			</section>
-		</PostContext.Provider>
+		</PostProvider>
 	);
 }
 
